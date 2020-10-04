58535dfc9698931f914978fee0faeae32b0df34c
Garv nanwani <garvnanwani16@gmail.com>
2020-10-04 12:59:00 +0530

    set up usercontext

---
+++ src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext(null);
+
+export const UserProvider = ({ children }) => {
+    const localSt = JSON.parse(localStorage.getItem('user'));
+    const [user, setUser] = useState(localSt ? localSt : null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
